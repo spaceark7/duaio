b3a4d6ddfd7f5900914e195e662dc02cf59d7fa5
SpaceArk& <spacearkchief@gmail.com>
2021-11-01 23:19:29 +0700

    detail need improvement

--- src/components/ProductBrief.js
+++ src/components/ProductBrief.js
@@ -2,40 +2,72 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsArrowUpRightSquareFill } from 'react-icons/bs'
 
-const ProductBrief = ({ title, desc, image, isGrey, link }) => {
+const ProductBrief = ({ title, desc, image, isGrey, link, imageTop }) => {
   return (
     <div
       className={`py-8 px-8 h-screen w-full  ${
         isGrey ? 'bg-gray-100' : 'bg-white'
       }  pb-8`}
     >
-      <div className='h-2/6'>
-        <h1
-          className={`font-sans pt-10 text-mGradientLeft
+      {imageTop ? (
+        <>
+          <div className='rounded-lg py-2 pt-4 h-4/6 '>
+            <img
+              className='rounded-lg h-full w-full object-cover object-center-bottom cursor-pointer  transition duration-700 transform hover:-translate-y-4  ease-out'
+              src={image}
+              alt=''
+            />
+          </div>
+          <div className='h-2/6'>
+            <h1
+              className={`font-sans pt-10 text-mGradientLeft
          font-bold  text-2xl  mx-auto pb-4`}
-        >
-          {title}
-        </h1>
-        <p className={`text-gray-700  leading-5 text-sm  mx-auto`}>{desc}</p>
-      </div>
+            >
+              {title}
+            </h1>
+            <p
+              className={`text-gray-700  leading-5 text-lg tracking-wide  mx-auto`}
+            >
+              {desc}
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className='h-2/6'>
+            <h1
+              className={`font-sans pt-10 text-mGradientLeft
+         font-bold  text-2xl  mx-auto pb-4`}
+            >
+              {title}
+            </h1>
+            <p className={`text-gray-700  leading-5 text-sm  mx-auto`}>
+              {desc}
+            </p>
+          </div>
 
-      <div className='pt-2 pb-4 '>
-        <Link
-          className='flex items-center gap-2 text-mGradientLeft transition '
-          to={link}
-        >
-          <h2 className=''>Lihat Selengkapnya</h2>
-          <BsArrowUpRightSquareFill className='hover:animate-bounce' />
-        </Link>
-      </div>
-
-      <div className='rounded-lg py-2 pt-4 h-3/6 '>
-        <img
-          className='rounded-lg h-full object-cover object-center-bottom cursor-pointer  transition duration-700 transform hover:-translate-y-4  ease-out'
-          src={image}
-          alt=''
-        />
-      </div>
+          {link ? (
+            <div className='pt-2 pb-4 '>
+              <Link
+                className='flex items-center gap-2 text-mGradientLeft transition '
+                to={link}
+              >
+                <h2 className=''>Lihat Selengkapnya</h2>
+                <BsArrowUpRightSquareFill className='hover:animate-bounce' />
+              </Link>
+            </div>
+          ) : (
+            <> </>
+          )}
+          <div className='rounded-lg py-2 pt-4 h-3/6 '>
+            <img
+              className='rounded-lg h-full object-cover object-center-bottom cursor-pointer  transition duration-700 transform hover:-translate-y-4  ease-out'
+              src={image}
+              alt=''
+            />
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -8,6 +8,7 @@ import { BiArrowBack } from 'react-icons/bi'
 import data_detail from '../data/DataDetail'
 import pagination from 'swiper/components/pagination'
 import { useHistory } from 'react-router'
+import ProductBrief from '../components/ProductBrief'
 
 SwiperCore.use([pagination])
 
@@ -30,7 +31,7 @@ const DetailPage = ({ match, history, location }) => {
     <div className='w-screen h-full'>
       <div
         style={{ backgroundImage: `url(/images/${detail.banner_path})` }}
-        className={`relative w-full h-screen  bg-cover bg-no-repeat bg-left`}
+        className={`relative w-full h-screen  bg-no-repeat bg-top-right`}
       >
         <div
           onClick={goBack}
@@ -38,6 +39,83 @@ const DetailPage = ({ match, history, location }) => {
         >
           <BiArrowBack className='text-mGradientLeft   text-xl' />
         </div>
+
+        <div className='py-20 px-8'>
+          <h1
+            className={`font-sans pt-10 text-white
+         font-light  text-6xl  mx-auto pb-4 pr-10 `}
+          >
+            Kendali Penuh Keamanan Secara Praktis & Efisien.
+          </h1>
+          <div className='w-full h-almost'></div>
+        </div>
+      </div>
+
+      {/* Body Section */}
+
+      <ProductBrief
+        imageTop={true}
+        title='Lebih Hemat & Terjangkau'
+        desc='Kami memberikan harga yang kompetitif dan membuka peluang bernegosiasi untuk menjalin kerjasama'
+        image='/images/money.jpg'
+      />
+      <ProductBrief
+        imageTop={true}
+        title='Selalu Siap Untuk Maintenance'
+        desc='Teknisi kami selalu siap untuk melakukan maintenance/pemeliharaan sistem CCTV anda, Serta pemeriksaan seluruh sistem yang berpotensi menjadi permasalahan dikemudian hari.'
+        image='/images/Maintenance_tech.jpg'
+      />
+
+      <ProductBrief
+        imageTop={true}
+        title='Akses Dimanapun Anda Berada'
+        desc='Sistem kami mendukung monitoring CCTV anda melalui ponsel pintar dan perangkat mobile lainnya. '
+        image='/images/smartphone_cctv.jpg'
+      />
+
+      <div className='w-full h-almost px-8'>
+        <Swiper
+          autoHeight
+          style={{
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+            '--swiper-navigation-size': '24px',
+          }}
+          slidesPerView={'auto'}
+          centeredSlides
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+        >
+          <SwiperSlide>
+            <div className='h-80 w-1/2 bg-business bg-cover px-12'>
+              <h1
+                className={`font-sans pt-10 text-white
+         font-light  text-3xl   mx-auto pb-4 pr-10 `}
+              >
+                Kami Menawarkan Harga Yang Terjangkau Untuk Anda.
+              </h1>
+              <p className='text-white pt-6 text-opacity-80 '>
+                Biaya yang kami tawarkan relatif murah dan kompetitif.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='h-80 w-1/3 bg-engineer  bg-cover px-12'>
+              <h1
+                className={`font-sans pt-10 text-white
+         font-light  text-3xl   mx-auto pb-4 pr-10 `}
+              >
+                Kualitas Terbaik Dalam Pengerjaan Menjadi Prioritas Utama Kami.
+              </h1>
+              <p className='text-white pt-6 text-opacity-80 '>
+                Serta pemeliharaan yang mudah dilakukan.
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   )
